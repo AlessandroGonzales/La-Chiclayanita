934f11100cb41a658b1970ba12b25b64d0c494d6
Alessandro <jamancapiero85@gmail.com>
2026-01-06 15:50:30 -0300

    React & Tailwin: First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-orange-50 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-orange-600 drop-shadow-sm">
+        ¡La Chiclayanita está en marcha! 🥘
+      </h1>
+    </div>
+  )
+}
+
+export default App
